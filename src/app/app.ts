18a2b9a5e3048c7c2a801dbc75ca06c65e18e307
John Webber <jlweb58@gmail.com>
2026-03-06 09:54:43 +0100

    Added GitHub Action Workflow

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,7 @@ import {MAT_TOOLTIP_DEFAULT_OPTIONS, MatTooltipDefaultOptions} from '@angular/ma
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {MatButton} from '@angular/material/button';
 import {MatToolbar} from '@angular/material/toolbar';
+import {MatCard, MatCardContent} from '@angular/material/card';
 
 /** Custom options the configure the tooltip's default show/hide delays. */
 export const myCustomTooltipDefaults: MatTooltipDefaultOptions = {
@@ -18,7 +19,7 @@ export const myCustomTooltipDefaults: MatTooltipDefaultOptions = {
   templateUrl: './app.html',
   styleUrl: './app.scss',
   providers: [{provide: MAT_TOOLTIP_DEFAULT_OPTIONS, useValue: myCustomTooltipDefaults}],
-  imports: [RouterOutlet, RouterLink, MatButton, RouterLinkActive, MatToolbar
+  imports: [RouterOutlet, RouterLink, MatButton, RouterLinkActive, MatToolbar, MatCardContent, MatCard
   ]
 })
 export class App {
