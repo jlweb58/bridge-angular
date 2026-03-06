8e3c9db85f822082f1bc5fea8141c1df9b51b88c
John Webber <jlweb58@gmail.com>
2026-03-06 08:37:43 +0100

    Added content for data privacy; removed separate Legal page

--- src/app/pages/legal-notice.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-legal-notice-page',
-  standalone: true,
-  templateUrl: './legal-notice.page.html',
-})
-export class LegalNoticePage {}
--- src/app/pages/privacy.page.ts
+++ src/app/pages/privacy.page.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, computed} from '@angular/core';
 
 @Component({
   selector: 'app-privacy-page',
   standalone: true,
   templateUrl: './privacy.page.html',
 })
-export class PrivacyPage {}
+export class PrivacyPage {
+  private readonly emailLocalPart = 'jlweb58';
+  private readonly emailDomain = 'gmail';
+  private readonly emailTld = 'com';
+
+  protected readonly emailText = computed(
+    () => `${this.emailLocalPart}@${this.emailDomain}.${this.emailTld}`,
+  );
+
+
+}
