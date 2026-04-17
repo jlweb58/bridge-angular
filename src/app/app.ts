473249d16a3332c73622a8fdb099d99eefec91eb
John Webber <jlweb58@gmail.com>
2026-04-17 15:35:11 +0200

    BRID-9 first step to working mobile version - hamburger menu instead of other menu

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,6 +5,9 @@ import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {MatButton} from '@angular/material/button';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatCard, MatCardContent} from '@angular/material/card';
+import {MatMenu, MatMenuItem, MatMenuModule, MatMenuTrigger} from '@angular/material/menu';
+import {MatDivider, MatDividerModule} from '@angular/material/divider';
+import {MatIcon} from '@angular/material/icon';
 
 /** Custom options the configure the tooltip's default show/hide delays. */
 export const myCustomTooltipDefaults: MatTooltipDefaultOptions = {
@@ -19,7 +22,7 @@ export const myCustomTooltipDefaults: MatTooltipDefaultOptions = {
   templateUrl: './app.html',
   styleUrl: './app.scss',
   providers: [{provide: MAT_TOOLTIP_DEFAULT_OPTIONS, useValue: myCustomTooltipDefaults}],
-  imports: [RouterOutlet, RouterLink, MatButton, RouterLinkActive, MatToolbar, MatCardContent, MatCard
+  imports: [RouterOutlet, RouterLink, MatButton, RouterLinkActive, MatToolbar, MatCardContent, MatCard, MatMenu, MatMenuTrigger, MatDivider, MatMenuItem, MatIcon
   ]
 })
 export class App {
